7af9ec268e8d32f6ae3d3e3ebcb625cb32a4e5ab
ronycse16b <rony.cse16b@gmail.com>
2022-09-29 09:05:11 +0600

    something change

---
+++ src/components/Activetiy/Activety.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Activety = () => {
+    return (
+        <div>
+            <h1>hello Activety</h1>
+        </div>
+    );
+};
+
+export default Activety;
---
+++ src/components/GymIteams/Gymitems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gymitems = () => {
+    return (
+        <div>
+             <h1> Hello this is gym</h1>
+        </div>
+    );
+};
+
+export default Gymitems;
